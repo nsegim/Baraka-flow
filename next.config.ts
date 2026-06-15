f74bbcdddbc11f55e0ca67c43d0ed26e33cbd955
Moussa <nsengimanamoussa3@gmail.com>
2026-06-15 12:51:00 +0200

    Prepare for production deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    // Allow images from these domains
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
